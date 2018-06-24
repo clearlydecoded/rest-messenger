3bdc5b58b3f71f59d2a81a0916e6c3cff1693c52
Yaakov <yaakov.chaikin@gmail.com>
2018-06-24 01:25:06 -0400

    Renamed package to rest-commander.

--- src/main/java/com/clearlydecoded/commander/Application.java
+++ src/main/java/com/clearlydecoded/commander/Application.java
@@ -3,7 +3,7 @@ package com.clearlydecoded.commander;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
