a0d7b147635f4949961cc7bf808559195f6942d9
Yaakov <yaakov.chaikin@gmail.com>
2018-06-24 19:30:39 -0400

    Minor cleanup. (#6)

--- src/main/java/com/clearlydecoded/commander/rest/CommandWithJustType.java
+++ src/main/java/com/clearlydecoded/commander/rest/CommandWithJustType.java
@@ -2,14 +2,12 @@ package com.clearlydecoded.commander.rest;
 
 import com.clearlydecoded.commander.Command;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.Data;
 
 /**
  * {@link CommandWithJustType} class represents a command to be used to deserialize the command
  * string coming as payload of the REST request. It is then used to determine which real concrete
  * {@link Command} implementation to deserialize the command string to.
  */
-//@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CommandWithJustType implements Command {
 
