45ddf765127b6f1515834faf1a6a07bc0ad8d54f
Yaakov <yaakov.chaikin@gmail.com>
2018-06-24 23:27:36 -0400

    Separated into library and example app to demo use. #5.

--- src/test/java/test/com/clearlydecoded/commander/discovery/registry/Command1Response.java
+++
@@ -1,7 +0,0 @@
-package test.com.clearlydecoded.commander.discovery.registry;
-
-import com.clearlydecoded.commander.CommandResponse;
-
-public class Command1Response implements CommandResponse {
-
-}
