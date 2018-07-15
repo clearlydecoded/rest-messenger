633173d94ae62eb04fd3211194ec45ea5d295c65
Yaakov <yaakov.chaikin@gmail.com>
2018-07-15 16:51:10 -0400

    Message model shows options for enums.
    Closes #19.

---
+++ src/test/java/test/com/clearlydecoded/messenger/documentation/EnumMessageResponse.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2018 Yaakov Chaikin (yaakov@ClearlyDecoded.com). Licensed under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in compliance with the License. You
+ * may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0. Unless required
+ * by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See
+ * the License for the specific language governing permissions and limitations under the License.
+ */
+package test.com.clearlydecoded.messenger.documentation;
+
+import com.clearlydecoded.messenger.MessageResponse;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * {@link EnumMessageResponse} class is a response to an enum containing message. This class is
+ * used to test generation of docs for a pojo that contains an enum, i.e., showing the options of
+ * the enum.
+ *
+ * @author Yaakov Chaikin (yaakov@ClearlyDecoded.com)
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EnumMessageResponse implements MessageResponse {
+
+  private String dummyResponse;
+}
